64851714ed9bf860d4cc7862166af757b07fe7d0
Yann <mandalorien.wootook@gmail.com>
2017-11-30 15:38:38 +0100

    add - extensions Zip

--- index.php
+++ index.php
@@ -7,7 +7,18 @@ require_once(ROOT_PATH .'librairy/class.form.php');# Outil d'affichage
 $template = new Template();
 $form = new Form($template);
 
+$titre = 'Extensions';
+if (!extension_loaded('zip')) {
+
+	$mess = 'L\'extension Zip n\' est pas active .';
+	$type = Form::MESS_WARNING;
+}else{
+	$mess = 'L\'extension Zip est active .';
+	$type = Form::MESS_NORMALE;
+}
+
 $parse = array();
+$parse['extensions'] = $form->Message($titre,$mess,$type);
 $parse['SCRIPT'] = SCRIPT;
 $form->display($template->displaytemplate('git_body', $parse));
 ?>
