cf85a619ab244752a7c0bfdbe3ec723746256d9b
Yann <mandalorien.wootook@gmail.com>
2017-11-30 10:12:42 +0100

    update script git.
    Warning : API rate limit exceeded for Your IP. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)
    
    Link : https://api.github.com/rate_limit

--- ajax/index.php
+++ ajax/index.php
@@ -5,55 +5,23 @@ require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'constant.php');
 require_once(ROOT_PATH .'librairy/class.tools.php');# outils divers
 require_once(ROOT_PATH .'librairy/class.template.php');# Base de donnees
 require_once(ROOT_PATH .'librairy/class.form.php');# Base de donnees
-require_once(ROOT_PATH .'librairy/class.pdo_connect.php');# Base de donnees
-require_once(ROOT_PATH .'librairy/class.model_class.php');# Gestion de création des classes ou de la BDD si les classes existent !
 
 $template = new Template();
 $form = new Form($template);
 
 $response = array();
-$errors = null;
-$text = null;
 
-if(isset($_POST['host']) && isset($_POST['user']) && isset($_POST['pass']) && isset($_POST['database']) && isset($_POST['prefix'])){
-	if(!empty($_POST['host']) && !empty($_POST['user']) && !empty($_POST['database'])){
-		
-		$PDO  = new DatabaseConnection($_POST['host'],$_POST['user'],$_POST['pass'],$_POST['database']);
-		if($PDO->Connexion() !== false){
-			$GENERATE = new Classes($PDO->Connexion());
-			
-			if (!empty($_POST['prefix'])) {
-				$GENERATE->set_Prefix($_POST['prefix']);
-			}
-			
-			$GENERATE->list_tables($PDO->Get_Database());
-			foreach($GENERATE->get_listTables() as $Tables){
-				
-				$GENERATE->set_Table($Tables);
-				$GENERATE->write_class($PDO->Get_Database(),true,true);
-				$text .= $GENERATE->removePrefix($Tables) ." <br>";
-			}
-			$response['errors'] = 0;
-			$response['message'] = $form->Message("Connexion à la base de donnée","connexion reussite et Génération des (".$GENERATE->get_NumbersTables().") tables :<br><span class='text-left'><code>".$text."</code></span>",Form::MESS_SUCCESS);
-			$response['tables_send'] = null;
+$error = isset($_POST['error']) ? $_POST['error']:false;
+if(isset($_POST['message'])){
+	if(!empty($_POST['message'])){
+		$message = htmlentities($_POST['message'],ENT_QUOTES);
+		if($error){
+			$response['message'] = $form->AlertMessage(NAME_SITE,$message,Form::ALERT_ALERT);
 		}else{
-			$response['errors'] = 1;
-			$response['message'] = $form->Message("Connexion à la base de donnée","connexion échoué : <br>". $PDO->Message() ,Form::MESS_WARNING);
-			$response['tables_send'] = null;
+			$response['message'] = $form->AlertMessage(NAME_SITE,$message,Form::ALERT_SUCCESS);
 		}
-	}else{
-		$response['errors'] = 1;
-		if(empty($_POST['host'])){ $errors .= "le champ <code>HOST</code> ,";}
-		if(empty($_POST['user'])){ $errors .= "le champ <code>USER</code> ,";}
-		if(empty($_POST['database'])){ $errors .= "le champ <code>DATABASE</code> ,";}
-		$errors = substr($errors,0,-1);
-		$response['message'] = $form->AlertMessage("Manque d'information","Veuillez saisir " .$errors .".",Form::ALERT_ALERT);
-		$response['tables_send'] = null;
 	}
-}else{
-	$response['errors'] = 1;
-	$response['message'] = $form->AlertMessage("titre","message",Form::ALERT_ALERT);
-	$response['tables_send'] = null;
 }
+
 echo json_encode($response);
 ?>
--- ressources/static/scripts/git.js
+++ ressources/static/scripts/git.js
@@ -5,29 +5,107 @@
  *
  * create 2015 by mandalorien
  */
+
+// configuration de base
+var gitconfig = {
+    Author:"kiwille",
+    Repository:"uniguerre",
+	listBranchFocus:[
+		"master",
+		"dev"
+	]
+};
+
 $(document).ready(function()
 {
-	// Assign handlers immediately after making the request,
-	// and remember the jqxhr object for this request
-	var listBranches = $.get("https://api.github.com/repos/kiwille/uniguerre/branches", function(data) {
+	$('#canvasloader-container').hide();
+	var cl = new CanvasLoader('canvasloader-container');
+		cl.setColor('#2970db'); /* default is '#000000' */
+		cl.setShape('sqare'); /* default is 'oval' */
+		cl.setDiameter(67); /* default is 40 */
+		cl.setDensity(20); /* default is 40 */
+		cl.setRange(1); /* default is 1.3 */
+		cl.setSpeed(1); /* default is 2 */
+		cl.setFPS(25); /* default is 24 */
+		cl.show(); /* Hidden by default */
+		
+	$("#searchRepository").keyup(function() {
+		console.log($( "#searchRepository" ).val());
+		gitconfig.Repository = $("#searchRepository").val();
+		
+		if($("#searchRepository").val().isAlphaNumeric()){
+			Search();
+		}
+	});
+	
+	RepositoryFocus();
+});
+
+function RepositoryFocus(){
+	var listBranches = $.get("https://api.github.com/repos/"+ gitconfig.Author +"/"+ gitconfig.Repository +"/branches", function(data) {
 	})
 	  .done(function(data) {
+		$("#message").hide();
 		$(data).each(function() {
-			if(this.name == 'master' || this.name =='dev')
-			{
-				var branch = this.name;
-				var SBranch = $.get("https://api.github.com/repos/kiwille/uniguerre/branches/"+ branch, function(data2) {
-				})
-				  .done(function(data2) {
-					console.log(data2.commit.commit.committer);
-				  })
-				  .fail(function() {
-					alert( "error" );
-				  });
+			for(var i = 0;i <= gitconfig.listBranchFocus.length - 1;i++){
+				if(gitconfig.listBranchFocus[i] == this.name)
+				{
+					var branch = this.name;
+					var SBranch = $.get("https://api.github.com/repos/"+ gitconfig.Author +"/"+ gitconfig.Repository +"/branches/"+ branch, function(infos) {
+					})
+					  .done(function(infos) {
+						console.log(infos.commit.commit.committer);
+					  })
+					  .fail(function() {
+						Message(data.responseJSON.message,true);
+					  });
+				}
 			}
 		});
 	  })
-	  .fail(function() {
-		alert( "error" );
+	  .fail(function(data) {
+		Message(data.responseJSON.message,true);
 	  });
-});
+}
+
+function Search(){
+	var listBranches = $.get("https://api.github.com/search/repositories?q="+ gitconfig.Repository, function(data) {
+	})
+	  .done(function(data) {
+		gitconfig.Repository = data.items[0].name;
+		console.log(gitconfig.Repository);
+		$("#message").hide();
+	  })
+	  .fail(function(data) {
+		Message(data.responseJSON.message);
+	  });
+}
+
+function Message(msg,error){
+	console.log(msg);
+	console.log(error);
+	$.post(getBaseURL()+"/ajax/index.php", {message:msg,error:error}, function(data) {
+			$('#canvasloader-container').show();
+	})
+	.done(function(data){
+		$('#canvasloader-container').show();
+		if(data.errors != 0){
+			$('#canvasloader-container').hide();
+			$("#message").show();
+			$("#message").html(data.message);
+		}else{
+			$('#canvasloader-container').hide();
+			$("#message").show();
+			$("#message").html(data.message);
+		}
+	})
+	.fail(function() {
+		$('#canvasloader-container').show();
+		console.log( "error" );
+	});
+}
+
+String.prototype.isAlphaNumeric = function() {
+  var regExp = /^[A-Za-z0-9]+$/;
+  return (this.match(regExp));
+};
